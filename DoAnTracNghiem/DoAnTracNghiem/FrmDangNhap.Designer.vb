e96ca967a2983c3425c9c9a8de926acc9b66eb25
Danh-3105 <Dan3105@github.com>
2023-03-26 20:45:03 +0700

    -- tao lai project khac

---
+++ DoAnTracNghiem/DoAnTracNghiem/FrmDangNhap.Designer.vb
@@ -0,0 +1,153 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+<Global.System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1726")> _
+Partial Class frmDangNhap
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+    Friend WithEvents LogoPictureBox As System.Windows.Forms.PictureBox
+    Friend WithEvents lbUsername As System.Windows.Forms.Label
+    Friend WithEvents lbPassword As System.Windows.Forms.Label
+    Friend WithEvents txtUsername As System.Windows.Forms.TextBox
+    Friend WithEvents txtPassword As System.Windows.Forms.TextBox
+    Friend WithEvents btnLogin As System.Windows.Forms.Button
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmDangNhap))
+        Me.LogoPictureBox = New System.Windows.Forms.PictureBox()
+        Me.lbUsername = New System.Windows.Forms.Label()
+        Me.lbPassword = New System.Windows.Forms.Label()
+        Me.txtUsername = New System.Windows.Forms.TextBox()
+        Me.txtPassword = New System.Windows.Forms.TextBox()
+        Me.btnLogin = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.cbServer = New System.Windows.Forms.ComboBox()
+        Me.lbServer = New System.Windows.Forms.Label()
+        CType(Me.LogoPictureBox, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'LogoPictureBox
+        '
+        Me.LogoPictureBox.Image = CType(resources.GetObject("LogoPictureBox.Image"), System.Drawing.Image)
+        Me.LogoPictureBox.Location = New System.Drawing.Point(0, 0)
+        Me.LogoPictureBox.Name = "LogoPictureBox"
+        Me.LogoPictureBox.Size = New System.Drawing.Size(165, 273)
+        Me.LogoPictureBox.TabIndex = 0
+        Me.LogoPictureBox.TabStop = False
+        '
+        'lbUsername
+        '
+        Me.lbUsername.Location = New System.Drawing.Point(198, 96)
+        Me.lbUsername.Name = "lbUsername"
+        Me.lbUsername.Size = New System.Drawing.Size(258, 23)
+        Me.lbUsername.TabIndex = 0
+        Me.lbUsername.Text = "&User name"
+        Me.lbUsername.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'lbPassword
+        '
+        Me.lbPassword.Location = New System.Drawing.Point(198, 153)
+        Me.lbPassword.Name = "lbPassword"
+        Me.lbPassword.Size = New System.Drawing.Size(273, 23)
+        Me.lbPassword.TabIndex = 2
+        Me.lbPassword.Text = "&Password"
+        Me.lbPassword.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'txtUsername
+        '
+        Me.txtUsername.Location = New System.Drawing.Point(198, 122)
+        Me.txtUsername.Name = "txtUsername"
+        Me.txtUsername.Size = New System.Drawing.Size(270, 22)
+        Me.txtUsername.TabIndex = 1
+        '
+        'txtPassword
+        '
+        Me.txtPassword.Location = New System.Drawing.Point(198, 179)
+        Me.txtPassword.Name = "txtPassword"
+        Me.txtPassword.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.txtPassword.Size = New System.Drawing.Size(270, 22)
+        Me.txtPassword.TabIndex = 3
+        '
+        'btnLogin
+        '
+        Me.btnLogin.Location = New System.Drawing.Point(216, 239)
+        Me.btnLogin.Name = "btnLogin"
+        Me.btnLogin.Size = New System.Drawing.Size(94, 23)
+        Me.btnLogin.TabIndex = 4
+        Me.btnLogin.Text = "&Đăng Nhập"
+        '
+        'btnExit
+        '
+        Me.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.btnExit.Location = New System.Drawing.Point(359, 239)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(94, 23)
+        Me.btnExit.TabIndex = 5
+        Me.btnExit.Text = "&Thoát"
+        '
+        'cbServer
+        '
+        Me.cbServer.FormattingEnabled = True
+        Me.cbServer.Location = New System.Drawing.Point(198, 50)
+        Me.cbServer.Name = "cbServer"
+        Me.cbServer.Size = New System.Drawing.Size(270, 24)
+        Me.cbServer.TabIndex = 6
+        '
+        'lbServer
+        '
+        Me.lbServer.Location = New System.Drawing.Point(195, 24)
+        Me.lbServer.Name = "lbServer"
+        Me.lbServer.Size = New System.Drawing.Size(258, 23)
+        Me.lbServer.TabIndex = 7
+        Me.lbServer.Text = "Server"
+        Me.lbServer.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'frmDangNhap
+        '
+        Me.AcceptButton = Me.btnLogin
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.CancelButton = Me.btnExit
+        Me.ClientSize = New System.Drawing.Size(500, 274)
+        Me.Controls.Add(Me.lbServer)
+        Me.Controls.Add(Me.cbServer)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnLogin)
+        Me.Controls.Add(Me.txtPassword)
+        Me.Controls.Add(Me.txtUsername)
+        Me.Controls.Add(Me.lbPassword)
+        Me.Controls.Add(Me.lbUsername)
+        Me.Controls.Add(Me.LogoPictureBox)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "frmDangNhap"
+        Me.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent
+        Me.Text = "FrmDangNhap"
+        CType(Me.LogoPictureBox, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents cbServer As ComboBox
+    Friend WithEvents lbServer As Label
+End Class
